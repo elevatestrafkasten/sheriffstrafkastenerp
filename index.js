92612eddb70049f6746b623400b137753b26927d
elevatestrafkasten <elevatelssd@gmail.com>
2025-10-20 14:41:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function searchFine() {
     let searchFor = document.getElementById("searchbar_input").value.toLocaleLowerCase()
-    
     let fines = document.querySelectorAll(".fine");
     for (var i = 0; i < fines.length; i++) {
         if (fines[i].querySelectorAll(".fineText")[0].innerHTML.toLocaleLowerCase().includes(searchFor)) {
@@ -10,7 +9,6 @@ function searchFine() {
             fines[i].classList.remove("showing")
             fines[i].classList.add("hiding")
         }
-        
     }
 }
 
@@ -19,18 +17,15 @@ function selectFine(event) {
     if (element.tagName == "FONT") return
     if (element.tagName == "TD") element = element.parentElement
     if (element.tagName == "I") element = element.parentElement.parentElement
-
     if (element.classList.contains("selected")) {
         element.classList.remove("selected")
     } else {
         element.classList.add("selected")
     }
-
     startCalculating()
 }
 
 function startCalculating() {
-
     document.getElementById("finesListTable").innerHTML = `<tr>
                     <th style="width: 80%;">Grund für die Geldstrafe</th>
                     <th style="width: 20%;">Bußgeld</th>
@@ -38,208 +33,89 @@ function startCalculating() {
 
     let fineResult = document.getElementById("fineResult")
     let fineAmount = 0
-
     let wantedResult = document.getElementById("wantedsResult")
     let wantedAmount = 0
-
     let characterResult = document.getElementById("charactersResult")
-
     let reasonResult = document.getElementById("reasonResult")
     let reasonText = ""
     let plate = document.getElementById("plateInput_input").value
-    let systemwanteds = document.getElementById("systemwantedsInput_input").value
     let blitzerort = document.getElementById("blitzerInput_input").value
-
     let infoResult = document.getElementById("infoResult")
     let noticeText = ""
     let removeWeaponLicense = false
     let removeDriverLicense = false
-
     let tvübergabe_org = document.getElementById("übergabeInput_select").value
     let tvübergabe_name = document.getElementById("übergabeInput_input").value
-
     let shortMode = false
-
     let fineCollection = document.querySelectorAll(".selected")
-    let fineCollectionWantedAmount = []
-    let fineCollectionFineAmount = []
 
     for (var i = 0; i < fineCollection.length; i++) { 
+        let cache_wanted_amount = parseInt(fineCollection[i].querySelector(".wantedAmount").getAttribute("data-wantedamount")) || 0
+        cache_wanted_amount += fineCollection[i].querySelector(".wantedAmount").querySelectorAll(".selected_extrawanted").length
 
+        const wantedAmountElement = fineCollection[i].querySelector(".wantedAmount");
+        if (cache_wanted_amount > 40) {
+            cache_wanted_amount = 40;
+            wantedAmountElement.style.color = "red";
+        } else {
+            wantedAmountElement.style.color = "";
+        }
 
-
-        let cache_wanted_amount = 0;
-
-        cache_wanted_amount = cache_wanted_amount + parseInt(fineCollection[i].querySelector(".wantedAmount").getAttribute("data-wantedamount"))
-        
-cache_wanted_amount = cache_wanted_amount + fineCollection[i].querySelector(".wantedAmount").querySelectorAll(".selected_extrawanted").length;
-
-const wantedAmountElement = fineCollection[i].querySelector(".wantedAmount");
-
-if (cache_wanted_amount > 40) {
-    cache_wanted_amount = 40;
-    wantedAmountElement.style.color = "red";
-} else {
-    wantedAmountElement.style.color = "";
-}
-
-
-        fineCollectionWantedAmount.push(cache_wanted_amount)
-
-
-        let cache_fine_amount = 0;
-
-        cache_fine_amount = cache_fine_amount + parseInt(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount"))
-
+        let cache_fine_amount = parseFloat(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount")) || 0
         let extrawanteds_found = fineCollection[i].querySelector(".wantedAmount").querySelectorAll(".selected_extrawanted")
-        let extrafines_amount = 0
         for (let b = 0; b < extrawanteds_found.length; b++) {
-            if (extrawanteds_found[b].getAttribute("data-addedfine")) cache_fine_amount = cache_fine_amount + parseInt(extrawanteds_found[b].getAttribute("data-addedfine"))
-            extrafines_amount = extrafines_amount + parseInt(extrawanteds_found[b].getAttribute("data-addedfine"))
+            if (extrawanteds_found[b].getAttribute("data-addedfine")) {
+                cache_fine_amount += parseFloat(extrawanteds_found[b].getAttribute("data-addedfine"))
+            }
         }
 
-        fineCollectionFineAmount.push(cache_fine_amount)
-
+        wantedAmount += cache_wanted_amount
+        fineAmount += cache_fine_amount
     }
-
-    console.log(fineCollectionWantedAmount);
-    
-    let maxWanted = fineCollectionWantedAmount[0]; // initialize to the first value
-
-    for (let i = 1; i < fineCollectionWantedAmount.length; i++) {
-        if (fineCollectionWantedAmount[i] > maxWanted) {
-            maxWanted = fineCollectionWantedAmount[i];
-        }
-    }
-
-    let maxFine = fineCollectionFineAmount[0]; // initialize to the first value
-
-    for (let i = 1; i < fineCollectionFineAmount.length; i++) {
-        if (fineCollectionFineAmount[i] > maxFine) {
-            maxFine = fineCollectionFineAmount[i];
-        }
-    }
-
-    wantedAmount = maxWanted
-    fineAmount = maxFine
-   
-    if (wantedAmount == undefined) wantedAmount = 0
-    if (fineAmount == undefined) fineAmount = 0
-    
-    console.log("Largest Wanteds:" + maxWanted);  
-    console.log("Largest Fine:" + maxFine);  
 
     for (var i = 0; i < fineCollection.length; i++) {
-        //fineAmount = fineAmount + parseInt(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount"))
-
         let extrawanteds_found = fineCollection[i].querySelector(".wantedAmount").querySelectorAll(".selected_extrawanted")
         let extrafines_amount = 0
         for (let b = 0; b < extrawanteds_found.length; b++) {
-            //if (extrawanteds_found[b].getAttribute("data-addedfine")) fineAmount = fineAmount + parseInt(extrawanteds_found[b].getAttribute("data-addedfine"))
-            extrafines_amount = extrafines_amount + parseInt(extrawanteds_found[b].getAttribute("data-addedfine"))
+            extrafines_amount += parseFloat(extrawanteds_found[b].getAttribute("data-addedfine"))
         }
 
-        //wantedAmount = wantedAmount + parseInt(fineCollection[i].querySelector(".wantedAmount").getAttribute("data-wantedamount"))
-        
-        //wantedAmount = wantedAmount + fineCollection[i].querySelector(".wantedAmount").querySelectorAll(".selected_extrawanted").length
-        //if (wantedAmount > 5) wantedAmount = 5
-        
+        let fineText = fineCollection[i].querySelector(".fineText").innerHTML.split("<i>")[0]
 
-        const d = new Date();
-        const localTime = d.getTime();
-        const localOffset = d.getTimezoneOffset() * 60000;
-        const utc = localTime + localOffset;
-        const offset = 2; // UTC of Germany Time Zone is +01.00
-        const germany = utc + (3600000 * offset);
-        let now = new Date(germany);
-
-        let hour = now.getHours();
-        if (hour < 10) hour = "0" + hour
-
-        let minute = now.getMinutes();
-        if (minute < 10) minute = "0" + minute
-
-        let day = now.getDate()
-        if (day < 10) day = "0" + day
-
-        let month = now.getMonth() + 1
-        if (month < 10) month = "0" + month
-
-        let fineText = ""
-        if (fineCollection[i].querySelector(".fineText").innerHTML.includes("<i>")) {
-            fineText = fineCollection[i].querySelector(".fineText").innerHTML.split("<i>")[0]
+        if (reasonText == "") {
+            reasonText = fineCollection[i].querySelector(".paragraph").innerHTML + " - " + fineText
         } else {
-            fineText = fineCollection[i].querySelector(".fineText").innerHTML
-        }
-
-        if (shortMode) {
-            if (reasonText == "") {
-                reasonText = `${day}.${month} ${hour}:${minute} - ${fineCollection[i].querySelector(".paragraph").hasAttribute("data-paragraphAddition") ? fineCollection[i].querySelector(".paragraph").getAttribute("data-paragraphAddition") + " " : ""}${fineCollection[i].querySelector(".paragraph").innerHTML}`
-            } else {
-                reasonText += ` + ${fineCollection[i].querySelector(".paragraph").hasAttribute("data-paragraphAddition") ? fineCollection[i].querySelector(".paragraph").getAttribute("data-paragraphAddition") + " " : ""}${fineCollection[i].querySelector(".paragraph").innerHTML}`
-            }
-        } else {
-            if (reasonText == "") {
-                reasonText = `${day}.${month} ${hour}:${minute} - ${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}`
-            } else {
-                reasonText += ` + ${day}.${month} ${hour}:${minute} - ${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}`
-            }
+            reasonText += " + " + fineCollection[i].querySelector(".paragraph").innerHTML + " - " + fineText
         }
 
         if (fineCollection[i].getAttribute("data-removedriverlicence") == "true") removeDriverLicense = true
         if (fineCollection[i].getAttribute("data-removeweaponlicence") == "true") removeWeaponLicense = true
 
-        
-
         if (fineCollection[i].classList.contains("addPlateInList")) {
-
             document.getElementById("finesListTable").innerHTML +=
-            `
-            <tr class="finesList_fine">
-                <td onclick="JavaScript:copyText(event)">${day}.${month} ${hour}:${minute} - ${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}${plate !== "" ? " - " + plate.toLocaleUpperCase() : ""}${blitzerort !== "" ? " - " + blitzerort : ""}</td>
-                <td>$${parseInt(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount")) + extrafines_amount}</td>
-            </tr>
-            `
+            `<tr class="finesList_fine">
+                <td onclick="JavaScript:copyText(event)">${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}${plate !== "" ? " - " + plate.toLocaleUpperCase() : ""}${blitzerort !== "" ? " - " + blitzerort : ""}</td>
+                <td>$${(parseFloat(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount")) + extrafines_amount).toLocaleString('de-DE', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}</td>
+            </tr>`
         } else {
             document.getElementById("finesListTable").innerHTML +=
-            `
-            <tr class="finesList_fine">
-                <td onclick="JavaScript:copyText(event)">${day}.${month} ${hour}:${minute} - ${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}</td>
-                <td>$${parseInt(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount")) + extrafines_amount}</td>
-            </tr>
-            `
-        }
-
-    }
-
-
-
-    if (removeDriverLicense) {
-        noticeText = "Führerschein entziehen"
-    }
-    if (removeWeaponLicense) {
-        if (noticeText =="") {
-            noticeText = "Waffenschein entziehen"
-        } else {
-            noticeText = noticeText + " + Waffenschein entziehen"
+            `<tr class="finesList_fine">
+                <td onclick="JavaScript:copyText(event)">${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}</td>
+                <td>$${(parseFloat(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount")) + extrafines_amount).toLocaleString('de-DE', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}</td>
+            </tr>`
         }
     }
 
-    if (tvübergabe_org !== "none" && tvübergabe_name !== "") {
-        reasonText += ` - @${tvübergabe_org.toLocaleUpperCase()} ${tvübergabe_name}`
-    }
-
+    if (removeDriverLicense) noticeText = "Führerschein entziehen"
+    if (removeWeaponLicense) noticeText = noticeText == "" ? "Waffenschein entziehen" : noticeText + " + Waffenschein entziehen"
+    if (tvübergabe_org !== "none" && tvübergabe_name !== "") reasonText += ` - @${tvübergabe_org.toLocaleUpperCase()} ${tvübergabe_name}`
 
     infoResult.innerHTML = `<b>Information:</b> ${noticeText}`
-fineResult.innerHTML = `<b>Geldstrafe:</b> <font style="user-select: all;">${fineAmount.toLocaleString('de-DE')}</font>`
+    fineResult.innerHTML = `<b>Geldstrafe:</b> <font style="user-select: all;">${fineAmount.toLocaleString('de-DE', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}%</font>`;
     wantedResult.innerHTML = `<b>Haftzeit:</b> <font style="user-select: all;">${wantedAmount}</font>`
     reasonResult.innerHTML = `<b>Grund:</b> <font style="user-select: all;" onclick="JavaScript:copyText(event)">${reasonText}</font>`
-    if (reasonText.length <= 150) {
-        characterResult.innerHTML = `<b>Zeichen:</b> ${reasonText.length}/150`
-    } else {
-        characterResult.innerHTML = `<b>Zeichen:</b> <font style="color: red;">${reasonText.length}/150<br>Dieser Grund ist zu lang!</font>`
-    }
-
+    if (reasonText.length <= 150) characterResult.innerHTML = `<b>Zeichen:</b> ${reasonText.length}/150`
+    else characterResult.innerHTML = `<b>Zeichen:</b> <font style="color: red;">${reasonText.length}/150<br>Dieser Grund ist zu lang!</font>`
 }
 
 
@@ -263,24 +139,17 @@ function showAttorneys() {
     }
 } 
 
-
 window.onload = async () => {
     let savedBody;
     let alreadyBig = true
-
     await sleep(Math.round(Math.random() * 2500))
-
     document.body.innerHTML = document.getElementById("scriptingDiv").innerHTML
     savedBody = document.body.innerHTML
-
     openDisclaimer()
-
     setInterval(() => {
         if (document.body.clientWidth < 700) {
             alreadyBig = false
-            document.body.innerHTML = `
-            <div style="transform: translate(-50%, -50%); font-weight: 600; font-size: 8vw; color: white; width: 80%; position: relative; left: 50%; top: 50%; text-align: center;">Diese Website kann nur auf einem PC angesehen werden<div>
-            `
+            document.body.innerHTML = `<div style="transform: translate(-50%, -50%); font-weight: 600; font-size: 8vw; color: white; width: 80%; position: relative; left: 50%; top: 50%; text-align: center;">Diese Website kann nur auf einem PC angesehen werden<div>`
             document.body.style.backgroundColor = "#121212"
         } else if (alreadyBig == false) {
             alreadyBig = true
@@ -294,14 +163,8 @@ function resetButton() {
     for (var i = 0; i < fineCollection.length; i++) {
         fineCollection[i].classList.remove("selected")
     }
-
-
-
-
     document.getElementById("notepadArea_input").value = ""
-    
     document.getElementById("reue_box").checked = false
-
     startCalculating()
 }
 
@@ -310,24 +173,15 @@ function resetErinnerung() {
     for (var i = 0; i < fineCollection.length; i++) {
         fineCollection[i].classList.remove("selected")
     }
-
-
-
     document.getElementById("notepadArea_input").value = ""
-    
     document.getElementById("reue_box").checked = false
-
     startCalculating()
 }
 
 function copyText(event) {
     let target = event.target
-    // Get the text field
     var copyText = target.innerHTML
-  
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.replace("<br>", ""));
-
+    navigator.clipboard.writeText(copyText.replace("<br>", ""))
     insertNotification("success", "Der Text wurde kopiert.", 5)
 }
 
@@ -336,59 +190,38 @@ function toggleExtraWanted(event) {
     let extrastarNumber = 0
     let isSelected = false
     let isLead = false
-
     if(target.classList.contains("extrawanted1")) extrastarNumber = 1
     if(target.classList.contains("extrawanted2")) extrastarNumber = 2
     if(target.classList.contains("extrawanted3")) extrastarNumber = 3
     if(target.classList.contains("extrawanted4")) extrastarNumber = 4
     if(target.classList.contains("extrawanted5")) extrastarNumber = 5
-
-
     if (target.classList.contains("selected_extrawanted")) isSelected = true
-
     if (isSelected && target.parentElement.querySelectorAll(".selected_extrawanted").length == extrastarNumber) isLead = true
-
     if (isSelected && isLead) {
-
-
         let foundEnabled = target.parentElement.querySelectorAll(".selected_extrawanted")
         for (let i = 0; i < foundEnabled.length; i++) {
             foundEnabled[i].classList.remove("selected_extrawanted")
-            
         }
-
         startCalculating()
         return
     }
-
     if (isSelected) {
-
-
         let found = target.parentElement.querySelectorAll(".extrawanted")
         for (let i = 0; i < found.length; i++) {
             if (i + 1 > extrastarNumber) {
-
                 found[i].classList.remove("selected_extrawanted")
             }
-            
         }
-
         startCalculating()
         return
     }
-
     if (!isSelected) {
         let found = target.parentElement.querySelectorAll(".extrawanted")
         for (let i = 0; i < extrastarNumber; i++) {
             found[i].classList.add("selected_extrawanted")
-            
         }
     }
-
     startCalculating()
-    //for (let index = 0; index < extrastarNumber; index++) {
-    //    const element = array[index];    
-    //}
 }
 
 setInterval(() => {
@@ -404,26 +237,18 @@ async function sleep(ms) {
 }
 
 async function disclaimerAccepted() {
-    // Disable Accept Button to prevent stacking of events
     document.getElementById("disclaimer_button").setAttribute("disabled", "")
-
     let disclaimerNode = document.getElementById("disclaimer")
     disclaimerNode.style.boxShadow = "rgba(0, 0, 0, 0.219) 0px 0px 70px 0vw"
-
     disclaimerNode.style.opacity = 0
     document.body.removeChild(document.getElementById("disclaimerBackgroundBlocker"))
-
     await sleep(1000)
-
     disclaimerNode.style.display = "none"
 }
 
 async function openDisclaimer() {
-    await sleep(500) // Let the page load
-
+    await sleep(500)
     let disclaimerNode = document.getElementById("disclaimer")
     disclaimerNode.style.opacity = 1
-
-
     disclaimerNode.style.boxShadow = "rgba(0, 0, 0, 0.219) 0px 0px 70px 30vw"
 }
